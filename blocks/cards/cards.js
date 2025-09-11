7d264177c0cc050feeddf96fec9491f51462a8ab
Tim Waring <waring@adobe.com>
2025-09-11 09:48:33 +0100

    updated cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -10,7 +10,29 @@ export default function decorate(block) {
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      else {
+        div.className = 'cards-card-body';
+        // Convert links to buttons with Sky.com styling
+        const links = div.querySelectorAll('a');
+        if (links.length > 0) {
+          // Group buttons together in a container
+          const buttonGroup = document.createElement('div');
+          buttonGroup.className = 'button-group';
+          
+          links.forEach((link, index) => {
+            link.classList.add('button');
+            if (index === 0) {
+              link.classList.add('primary');
+            } else {
+              link.classList.add('secondary');
+            }
+            buttonGroup.appendChild(link);
+          });
+          
+          // Add button group to the card body
+          div.appendChild(buttonGroup);
+        }
+      }
     });
     ul.append(li);
   });
